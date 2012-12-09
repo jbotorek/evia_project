a4a15ae77c51481021481d6a320834478a57584e
jbotorek <botorekj@gmail.com>
2012-12-09 12:42:41 +0100

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Evia::Application.config.secret_token = '87ef0699a0addd1559ccf3d6623c01ee31b681066bd9352f2953a3bf689d21aa9d6d15c1b56450874bbc806a2c24ebf57ffc0ba6f4e65068c3c4be458bdd82cd'
