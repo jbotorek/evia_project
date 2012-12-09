637c3ff366c58f09aa7fbfb8a8d7ef9cc3b00028
jbotorek <botorekj@gmail.com>
2012-12-09 14:10:57 +0100

    User model + gravatar and authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
 class User < ActiveRecord::Base
-  attr_accessible :email
+  attr_accessible :email, :password, :password_confirmation
+  has_secure_password
+  
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: 	true,
+					format: 	{ with: VALID_EMAIL_REGEX },
+					uniqueness: { case_sensitive: false }
+					
+  before_save { |user| user.email = email.downcase }
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
+  
 end
